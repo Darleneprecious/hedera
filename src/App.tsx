d71a13335eadeba940a91f6adf9f613c5908a63f
Darlene precious <preciousayewa4@gmail.com>
2025-10-28 13:46:48 +0100

    vercel

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Screen1 from './onboarding/screen1';
 import Screen2 from './onboarding/screen2';
-import { AuthScreen } from './page/screen3';
+// import { AuthScreen } from './page/screen3';
 import Screen5 from './onboarding/screen5';
-import Screen4 from './onboarding/screen4';
+// import Screen4 from './onboarding/screen4';
 
-import Screen7 from './onboarding/screen7';
+// import Screen7 from './onboarding/screen7';
 
 function App() {
   return (
@@ -18,10 +18,10 @@ function App() {
         {/* Onboarding flow */}
         <Route path="/" element={<Screen1 />} />
         <Route path="/welcome" element={<Screen2 />} />
-        <Route path="/auth" element={<AuthScreen />} />
-        <Route path="/verify-code" element={<Screen4 />} />
+        {/* <Route path="/auth" element={<AuthScreen />} /> */}
+        {/* <Route path="/verify-code" element={<Screen4 />} /> */}
         <Route path="/signup" element={<Screen5 />} />
-        <Route path="/login" element={<Screen7 />} />
+        {/* <Route path="/login" element={<Screen7 />} /> */}
 
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
